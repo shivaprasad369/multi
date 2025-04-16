92e6ec75783754ab044810358cb73d78e983e79e
shivaprasad sapare <122955366+shivaprasad369@users.noreply.github.com>
2025-04-16 12:43:46 +0530

    multi

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+   <div>
+    <p className="text-rose-500">
+      hello world
+    </p>
+   </div>
+  );
+}
