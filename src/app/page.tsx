ff81814ba582602daf46465f940dc2b5cd0b8e7a
shivaprasad sapare <122955366+shivaprasad369@users.noreply.github.com>
2025-04-16 13:50:22 +0530

    'components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,31 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
+import { Check } from "lucide-react";
 
 
 export default function Home() {
   return (
-   <div>
-    <p className="text-rose-500">
-      hello world
-    </p>
+   <div className="p-3 flex flex-col gap-3">
+    <div>
+<Input placeholder="I am input"/>
+
+    </div>
+    <div className="">
+      <Textarea placeholder="i am textarea"/>
+    </div>
+    <div className="">
+      <Button variant={'elevated'}>Button</Button>
+
+    </div>
+    <Progress/>
+    <div>
+      <Checkbox/>
+    </div>
+  
    </div>
   );
 }
